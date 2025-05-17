d3fc730dc75bbd8c9edc278b86832f1d5854f0c6
anastasia <an.sssst@mail.ru>
2025-05-17 15:10:15 +0300

    Initial Commit

---
+++ DronMissionsApp/DronMissionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DronMissionsAppApp.swift
+//  DronMissionsApp
+//
+//  Created by Анастасия Сергеева on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DronMissionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
